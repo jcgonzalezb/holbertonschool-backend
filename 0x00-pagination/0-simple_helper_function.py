7e2bb8e25f4179e99f6c426ef8d9754864a71202
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-05 18:30:02 -0500

    PEP8 fixed

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 """
-Main file
+This project module contains a function that
+takes two integer arguments and returns a tuple.
 """
 
 
 def index_range(page, page_size):
+    """
+    Function that takes two integer arguments and
+    returns a tuple.
+    Returns:
+        A tuple of size two containing a start index
+        and an end index corresponding to the range
+        of indexes to return in a list for those
+        particular pagination parameters.
+    """
     tuple1 = ()
     tuple2 = ()
     end_index = page * page_size
-    tuple2 +=(end_index, )
+    tuple2 += (end_index, )
     start_index = end_index - page_size
-    tuple1 +=(start_index, )
+    tuple1 += (start_index, )
 
     return tuple1 + tuple2
