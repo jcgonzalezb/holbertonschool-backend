49c82630c27644cc17de82b9a75785a574a0cf1f
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-05 22:22:18 -0500

    initial version

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+This project module contains a function that
+takes two integer arguments and returns a tuple.
+"""
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        xxxxxx
+        Returns:
+            xxxxxx
+        """
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page > 0
+        assert page_size > 0
+
+        index_range(page, page_size)
+        print(Server.dataset)
+        
+
+def index_range(page, page_size):
+    """
+    Function that takes two integer arguments and
+    returns a tuple.
+    Returns:
+        A tuple of size two containing a start index
+        and an end index corresponding to the range
+        of indexes to return in a list for those
+        particular pagination parameters.
+    """
+    tuple1 = ()
+    tuple2 = ()
+    end_index = page * page_size
+    tuple2 += (end_index, )
+    start_index = end_index - page_size
+    tuple1 += (start_index, )
+
+    return tuple1 + tuple2
