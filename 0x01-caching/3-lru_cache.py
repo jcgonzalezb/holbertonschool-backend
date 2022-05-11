2bd924300e1af0010631de01d77a76670450ab62
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-11 12:00:37 -0500

    main file

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" LIFOCacheCache module to work with a basic dictionary.
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache inherits from BaseCaching and
+    is a caching system.
+    """
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        dict_data = self.cache_data
+        if not (key is None or item is None):
+            if (len(dict_data) == BaseCaching.MAX_ITEMS and
+                    key not in dict_data):
+                last = sorted(dict_data.keys())[-1]
+                print("DISCARD: {}".format(last))
+                del dict_data[last]
+                dict_data[key] = item
+            else:
+                dict_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        dict_data = self.cache_data
+        if key is None or key not in dict_data:
+            return None
+
+        return dict_data[key]
