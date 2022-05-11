2ce98f311421b2be4a22d712a77ab7c76dd93a7c
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-11 10:23:34 -0500

    if added

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -17,9 +17,10 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        dict_data = self.cache_data
-        dict_data[key] = item
-        return dict_data
+        if not (key is None or item is None):
+            dict_data = self.cache_data
+            dict_data[key] = item
+            return dict_data
 
     def get(self, key):
         """ Get an item by key
