7bd4f78c2917b7ed2c3603010f2e5d4279f60525
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-11 22:11:47 -0500

    get method fixed

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -13,21 +13,30 @@ class LRUCache(BaseCaching):
         """ Initiliaze
         """
         super().__init__()
+        self.ordered_cache_keys = []
 
     def put(self, key, item):
         """ Add an item in the cache
         """
         dict_data = self.cache_data
         if not (key is None or item is None):
-            if(
-                len(dict_data) == BaseCaching.MAX_ITEMS and
-                key not in dict_data
+            if (len(dict_data) == BaseCaching.MAX_ITEMS and
+                    key not in dict_data):
+                lru = self.ordered_cache_keys[0]
+                print('DISCARD: {}'.format(lru))
+                self.ordered_cache_keys.pop(0)
+                del dict_data[lru]
+                dict_data[key] = item
+                self.ordered_cache_keys.append(key)
+            elif (
+                len(dict_data.keys()) == BaseCaching.MAX_ITEMS and
+                (key in dict_data.keys())
             ):
-                fi = sorted(dict_data.keys())[0]
-                print("DISCARD: {}".format(fi))
-                del dict_data[fi]
+                self.ordered_cache_keys.remove(key)
+                self.ordered_cache_keys.append(key)
                 dict_data[key] = item
             else:
+                self.ordered_cache_keys.append(key)
                 dict_data[key] = item
 
     def get(self, key):
