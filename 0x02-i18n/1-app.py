99de43b0db66167144d1e20d5a264ec59d8576b9
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-17 12:19:05 -0500

    app.config fixed

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -12,18 +12,19 @@ babel = Babel(app)
 @app.route('/', methods=("GET", "POST"), strict_slashes=False)
 def index():
     """Function that displays info from 1-index.html"""
-    return render_template(
-        '1-index.html')
+    return render_template('1-index.html')
 
 
 class Config(object):
+    """flask app config Class"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config.from_object('1-app.Config')
+#app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+#app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 if __name__ == '__main__':
     app.run()
