33fe3f5a929bf1ae7400a87469324285fb7331eb
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-17 12:22:40 -0500

    comment fixed

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Script that starts a basic Flask app
+Script that starts a basic flask Babel setup
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel
@@ -11,23 +11,23 @@ babel = Babel(app)
 
 @app.route('/', methods=("GET", "POST"), strict_slashes=False)
 def index():
-    """Function that displays info from 1-index.html"""
-    return render_template(
-        '2-index.html')
+    """Function that displays info from 2-index.html"""
+    return render_template('2-index.html')
 
 
 class Config(object):
+    """flask app config Class"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config.from_object('1-app.Config')
 
 
 @babel.localeselector
 def get_locale():
+    """Function that determine the best match with supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
