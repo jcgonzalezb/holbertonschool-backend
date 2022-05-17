f8dc53fba902b3024d9b1878ab7124fbb47231d0
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-17 10:45:14 -0500

    render fixed

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,14 +2,16 @@
 """
 Script that starts a basic Flask app
 """
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', methods=("GET", "POST"), strict_slashes=False)
 def index():
-    """Function that displays Hello world'"""
-    return "Hello world"
+    """Function that displays info from 0-index.html"""
+    return render_template(
+        '0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
