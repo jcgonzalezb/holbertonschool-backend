2b87585561429cccac1d18b340667f57f7e379e2
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-17 12:03:23 -0500

    syntax fixed

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 @app.route('/', methods=("GET", "POST"), strict_slashes=False)
 def index():
     """Function that displays info from 0-index.html"""
-    return render_template(
-        '0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
