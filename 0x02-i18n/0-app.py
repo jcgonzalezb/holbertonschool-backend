f0c01b24d4bc7ae9e296d498f9f9f97c8c32782c
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-17 10:36:22 -0500

    return fixed

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', methods=("GET", "POST"), strict_slashes=False)
 def index():
     """Function that displays Hello world'"""
-    return "Hello HBNB!"
+    return "Hello world"
 
 if __name__ == '__main__':
     app.run()
