e81ed4cb174e16ed5da520861e69c8e549fa1968
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-17 10:48:17 -0500

    initial version

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Script that starts a basic Flask app
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', methods=("GET", "POST"), strict_slashes=False)
+def index():
+    """Function that displays info from 0-index.html"""
+    return render_template(
+        '0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
