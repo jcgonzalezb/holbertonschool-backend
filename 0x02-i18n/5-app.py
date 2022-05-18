43853d8df9deaeb0d0ac0b533ad30d5a90f71ee9
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-17 21:35:37 -0500

    get_user updated

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """
-Script that starts a basic user login system
+Script that starts a basic mock user login system
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
+
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -11,8 +12,8 @@ babel = Babel(app)
 
 @app.route('/', methods=("GET", "POST"), strict_slashes=False)
 def index():
-    """Function that displays info from 4-index.html"""
-    return render_template('4-index.html', locale=get_locale()
+    """Function that displays info from 5-index.html"""
+    return render_template('5-index.html', locale=get_locale()
                            or babel.default_locale)
 
 
@@ -23,7 +24,7 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object('4-app.Config')
+app.config.from_object('5-app.Config')
 
 
 @babel.localeselector
@@ -34,6 +35,32 @@ def get_locale():
         return locale
     else:
         return request.accept_languages.best_match(Config.LANGUAGES)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+def get_user():
+    """Function that returns an user dictionary"""
+    user = request.args.get('login_as')
+    key = int(user)
+    if key is not None and key in users:
+        return users[key]
+    else:
+        return None
+
+@app.before_request
+def before_request():
+    """Function that returns an user dictionary"""
+
+
+    g.user = user
+
+
+
 
 
 if __name__ == '__main__':
